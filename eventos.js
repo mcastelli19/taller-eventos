f0b19ffe5c3bf172f69a1abd5fba676561c97120
mcastelli19 <maiacastelli19@gmail.com>
2023-08-31 21:08:23 -0300

    div

---
+++ eventos.js
@@ -0,0 +1,12 @@
+const saludarButton = document.getElementById("saludarButton");
+
+saludarButton.addEventListener("click", function() {
+  alert("Hola!");
+});
+
+
+const divSaludo = document.getElementById("divSaludo");
+
+divSaludo.addEventListener("click", function() {
+  alert("Hola! Soy el div");
+});
